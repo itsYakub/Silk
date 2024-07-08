f6ae48b04c74a04ddb41be81fa27f92b4d4fc570
Jakub Oleksiak <yakubofficialmail@gmail.com>
2024-07-08 08:57:17 +0200

    Image manipulation API
    Added some image-manipultion functions and redesigned the example for console rendering. Some other functionality (i.e. alpha-blending) was tweaked.

--- examples/example_console/main.c
+++ examples/example_console/main.c
@@ -1,11 +1,12 @@
 // Compilation command:
 // cc main.c -o example.out
 
+#include <unistd.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #define SILK_PIXELBUFFER_WIDTH 64
-#define SILK_PIXELBUFFER_HEIGHT 32
+#define SILK_PIXELBUFFER_HEIGHT 48
 
 #define SILK_IMPLEMENTATION
 #include "../../silk.h"
@@ -22,40 +23,51 @@ i32 ConsoleBlit(pixel* buf) {
             pixel pix = silkGetPixel(
                 buf, 
                 (vec2i) { x, y }, 
-                (vec2i) { SILK_PIXELBUFFER_WIDTH, SILK_PIXELBUFFER_HEIGHT}
+                SILK_PIXELBUFFER_WIDTH
             );
 
             if(pix == 0x00000000) {
-                printf("%c", ' ');
+                putc(' ', stdout);
+                putc(' ', stdout);
             }
 
             else {
-                printf("%c", '#');
+                putc('#', stdout);
+                putc('#', stdout);
             }
         }
 
-        printf("\n");
+        putc('\n', stdout);
     }
+
+    printf("\033[%dA", SILK_PIXELBUFFER_HEIGHT);
+    printf("\033[%dD", SILK_PIXELBUFFER_WIDTH);
+
 
     return SILK_SUCCESS;
 }
 
 i32 main(int argc, const string argv[]) {
     pixel buffer[SILK_PIXELBUFFER_WIDTH * SILK_PIXELBUFFER_HEIGHT];
+    f32 rotation = 0.0f;
 
-    silkClearPixelBuffer(buffer);
-    silkDrawRectPro(
-        buffer, 
-        (vec2i) { SILK_PIXELBUFFER_WIDTH, SILK_PIXELBUFFER_HEIGHT },
-        SILK_PIXELBUFFER_WIDTH, 
-        (vec2i) { SILK_PIXELBUFFER_WIDTH / 2, SILK_PIXELBUFFER_HEIGHT / 2 }, 
-        (vec2i) { SILK_PIXELBUFFER_HEIGHT / 4, SILK_PIXELBUFFER_HEIGHT / 4 }, 
-        0,
-        (vec2i) { SILK_PIXELBUFFER_HEIGHT / 8, SILK_PIXELBUFFER_HEIGHT / 8 },
-        0xffffffff
-    );
+    while(true) {
+        silkClearPixelBuffer(buffer);
+        silkDrawRectPro(
+            buffer, 
+            (vec2i) { SILK_PIXELBUFFER_WIDTH, SILK_PIXELBUFFER_HEIGHT },
+            SILK_PIXELBUFFER_WIDTH, 
+            (vec2i) { SILK_PIXELBUFFER_WIDTH / 2, SILK_PIXELBUFFER_HEIGHT / 2 }, 
+            (vec2i) { SILK_PIXELBUFFER_HEIGHT / 4, SILK_PIXELBUFFER_HEIGHT / 4 }, 
+            rotation += 0.05f,
+            (vec2i) { SILK_PIXELBUFFER_HEIGHT / 8, SILK_PIXELBUFFER_HEIGHT / 8 },
+            0xffffffff
+        );
 
-    ConsoleBlit(buffer);
+        ConsoleBlit(buffer);
+
+        usleep(1000 / 60);
+    }
 
     return 0;
 }
